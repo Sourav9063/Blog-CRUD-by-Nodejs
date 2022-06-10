a25155c42d0106e64918327ed2e81a2017d7531e
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2022-06-11 01:00:03 +0600

    prblm

--- src/controllers/blog_controller.js
+++ src/controllers/blog_controller.js
@@ -1,4 +1,17 @@
+const Blog = require('../models/blog_model');
+
+
+
 exports.getBlogList = (req, res) => {
 
-    console.log('getBlogList');
+    // console.log('getBlogList');
+    Blog.getAllBlogs((err, blogs) => {
+        console.log(blogs);
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.json(blogs);
+        }
+    })
 }
